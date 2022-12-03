9e40fbeedab47ba28b916cace0e246df58e71c88
aliakbaresmaeili98 <aliakbaresmaeili98@gmail.com>
2022-12-03 17:53:45 +0330

    create new project

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslateModule } from '@ngx-translate/core';
+import { BaseImportsModule } from 'src/app/shared/modules/base-imports/base-imports.module';
+import { CustomCardsModule } from 'src/app/shared/modules/custom-cards/custom-cards.module';
+import { SwiperHeaderModule } from 'src/app/shared/modules/swiper-header/swiper-header.module';
+import { SwiperModule } from 'swiper/angular';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+const MODULES = [
+  CommonModule,
+  HomeRoutingModule,
+  MatIconModule,
+  MatButtonModule,
+  FlexLayoutModule,
+  TranslateModule.forChild(),
+  SwiperModule,
+  MatToolbarModule,
+  MatMenuModule,
+  CustomCardsModule,
+  MatDividerModule,
+  MatListModule,
+  BaseImportsModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatInputModule,
+  SwiperHeaderModule,
+];
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [MODULES],
+})
+export class HomeModule {}
